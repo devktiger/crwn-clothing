ae78273c1c2386442d59c4301d72c29084826724
Andrew <dev.andrewhan@gmail.com>
2019-07-17 20:30:25 -0400

    test2

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -26,7 +26,7 @@ const HomePage = () => (
       <div className="menu-item">
         <div className="content">
           <h1 className="title">WOMENS</h1>
-          <span className="subtitle">SHOP NOW</span>
+          <span className="subtitle">SHOP NOW222</span>
         </div>
       </div>
       <div className="menu-item">
